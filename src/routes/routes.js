ed755dabd559ba22aef669b6db83f74752f1a4a1
MaazKhan <maxiikhan08@gmail.com>
2023-08-26 23:30:39 +0500

    path changes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,8 +6,13 @@ import AppLayout from '../layout/layout';
 // Pages
 import HomePage from '../pages/home/home';
 import CompletedTodosPage from '../pages/completed-todos/completed-todos';
+import { Navigate } from 'react-router';
 
 export const APP_ROUTES = [
+  {
+    path: '/',
+    element: <Navigate to='home' />,
+  },
   {
     path: '/',
     element: <AppLayout />,
